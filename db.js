9e076f65e23eb0764a2e9c19d9df593497c1b582
deadlauncherg <113706114+deadlauncherg@users.noreply.github.com>
2025-08-26 09:44:57 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,29 @@
-// db.js
-function connectDB() {
-  console.log("⚡ Database not configured yet.");
-}
-module.exports = { connectDB };
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("./database.sqlite");
+
+db.serialize(() => {
+  db.run("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT UNIQUE, password TEXT)");
+});
+
+module.exports = {
+  getUser: (username) => new Promise((resolve, reject) => {
+    db.get("SELECT * FROM users WHERE username = ?", [username], (err, row) => {
+      if (err) reject(err);
+      else resolve(row);
+    });
+  }),
+
+  getUserById: (id) => new Promise((resolve, reject) => {
+    db.get("SELECT * FROM users WHERE id = ?", [id], (err, row) => {
+      if (err) reject(err);
+      else resolve(row);
+    });
+  }),
+
+  createUser: (username, password) => new Promise((resolve, reject) => {
+    db.run("INSERT INTO users (username, password) VALUES (?, ?)", [username, password], function (err) {
+      if (err) reject(err);
+      else resolve(this.lastID);
+    });
+  })
+};
